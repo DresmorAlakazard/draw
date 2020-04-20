03b4e5894b9c732309cffbe947a1c076ee1d225c
Márton Dávid Orosz <marton.david.orosz@gmail.com>
2020-04-20 13:25:42 +0200

    xcpara

--- rule.h
+++ rule.h
@@ -1,6 +1,39 @@
 #ifndef RULE_H
 #define RULE_H
 
+#include <math.h>
+
+#define abs(v) (v > 0.0 ? v : -v)
+#define min(a, b) (a < b ? a : b)
+#define max(a, b) (a > b ? a : b)
+
+/* OTHER RULES BEGIN *********************************************************/
+
+static void xcpara (
+    unsigned char *map,
+    int iframe,
+    double fframe,
+    unsigned char *pixel,
+    int ix, int iy,
+    double fx, double fy
+) {
+    int i;
+    double r;
+    double n = floor (log (abs (fy)) / log (2.0));
+
+    for (i = 0; i < 256; ++i, ++n) {
+        r = pow (2.0, n);
+
+        if (pow (fmod (abs (fx), 2.0 * r) - r, 2.0) + pow (fy, 2.0) <= pow (r, 2.0)) {
+            break;
+        }
+    }
+
+    pixel[0] = fmod (n / 5.0, 1.0) * 255;
+}
+
+/* OTHER RULES END ***********************************************************/
+
 void rule (
     unsigned char *map,
     int iframe,
@@ -9,12 +42,12 @@ void rule (
     int ix, int iy,
     double fx, double fy
 ) {
-    if (fx * fx + fy * fy <= 1.0) {
-        pixel[0] = 255;
-        pixel[3] = 255;
-    } else {
-        pixel[3] = 0;
-    }
+    pixel[0] = 0;
+    pixel[1] = 0;
+    pixel[2] = 0;
+    pixel[3] = 255;
+
+    xcpara (map, iframe, fframe, pixel, ix, iy, fx, fy);
 }
 
 #endif
